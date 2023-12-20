39cdc7d9bd9237fad571a0d17ff91d7a34fe70dc
Drones507 <114556741+Drones507@users.noreply.github.com>
2023-12-20 09:29:41 -1000

    Update bats.cpp
    Polymorphic child bat class

--- bats.cpp
+++ bats.cpp
@@ -1,7 +1,19 @@
-#include "bats.h"
-
 #include <iostream>
+#include "bats.h"
 
 using namespace std;
 
+Bats::Bats(char B)
+	: Event(B){
+}
+void Bats::action(){
+	confused = true;
+}
+char Bats::get_sprite(){
+	return sprite;
+}
+string Bats::get_percept(){
+	// percept = "\n ! You hear wings flapping...\n";
+	return percept;
+}
 //Bats Implementation
