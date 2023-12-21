dbbd39c1c61d249581eeff55fbf59b716afbd5c8
Drones507 <114556741+Drones507@users.noreply.github.com>
2023-12-20 16:45:28 -1000

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -1,15 +1,39 @@
 #include "room.h"
 
 using namespace std;
-
-// void Room::set_event(Event* new_event){
-	// if(this-> != nullptr){
-	// 	delete this->e;
-	// }
-	// this->e = new_event;
-// }
-
-// Event* Room::get_event() const;
-	// return this->e;
-
-//Room Implementation
+Room::Room(){
+	this->e = nullptr;
+}
+Room::~Room(){
+	if(this->e != nullptr){
+		delete this->e;
+	}
+}
+//CC
+Room::Room(Event* new_event){
+	this->e == new_event;
+}
+//AOO
+//implement...
+void Room::set_event(Event* new_event){
+	if(this->e != nullptr){
+		delete this->e;
+	}
+	this->e = new_event;
+}
+Event* Room::get_event(){
+	return this->e;
+}
+char Room::get_sprite(){
+	return e->get_sprite();
+}
+bool Room::check_event(){
+	if(this->e == nullptr){
+		cout << "There is no event";
+		return false;
+	}
+	else{
+	return true;
+	cout << "This room has an event";
+}
+}
