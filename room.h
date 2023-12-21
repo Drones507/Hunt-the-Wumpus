55d7ad157abd7701884ec783621391959a59e464
Drones507 <114556741+Drones507@users.noreply.github.com>
2023-12-20 17:31:22 -1000

    Updated room.h

--- room.h
+++ room.h
@@ -1,24 +1,39 @@
-#ifndef ROOM_H
-#define ROOM_H 
-
-#include "event.h"
+#include "room.h"
 
 using namespace std;
-
-//Room Interface
-//Note: Each room may be empty or has an event (bat, gold, pit, or wumpus);
-//		Use event polymorphically
-
-class Room
-{
-private: 
-	Event *e;
-	//EX: If the player gets the goal, it needs to despawn which I can do by setting the pointer to null
-
-public:
-	void set_event(Event *new_event);
-	Event* get_event() const;
-
-};
-
-#endif
+Room::Room(){
+	this->e = nullptr;
+}
+Room::~Room(){
+	if(this->e != nullptr){
+		delete this->e;
+	}
+}
+//CC
+Room::Room(Event* new_event){
+	this->e == new_event;
+}
+//AOO
+//implement...
+void Room::set_event(Event* new_event){
+	if(this->e != nullptr){
+		delete this->e;
+	}
+	this->e = new_event;
+}
+Event* Room::get_event(){
+	return this->e;
+}
+char Room::get_sprite(){
+	return e->get_sprite();
+}
+bool Room::check_event(){
+	if(this->e == nullptr){
+		cout << "There is no event";
+		return false;
+	}
+	else{
+	return true;
+	cout << "This room has an event";
+}
+}
