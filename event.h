0943368b1b03ea2f30a4bc3d6057534b52f0fa53
Drones507 <114556741+Drones507@users.noreply.github.com>
2023-12-20 16:38:18 -1000

    Update event.h

--- event.h
+++ event.h
@@ -1,20 +1,59 @@
 #ifndef EVENT_H
 #define EVENT_H 
 
+#include <string>
+#include <iostream>
 using namespace std;
 
 //Event Interface
 //Note: this must be an abstract class!
 
-class Event
-{
-
+class Event{
 protected: 
-	//xCord
-	//yCord
-	// Wumpus *w
+	char sprite;
+	string percept;
 public:
-	
-	
+/*********************************************************************
+** Function: Event non-defualt constructor 
+** Description: Constructs all Event class variables for every member class 
+** Parameters: char
+** Pre-Conditions: none  
+** Post-Conditions: none
+*********************************************************************/
+	Event(char sprite);
+/*********************************************************************
+** Function: set_sprite
+** Description: 
+** Parameters: 
+** Pre-Conditions:
+** Post-Conditions:
+*********************************************************************/
+	void set_sprite(char e);
+/*********************************************************************
+** Function: set_percept
+** Description: 
+** Parameters: 
+** Pre-Conditions:
+** Post-Conditions:
+*********************************************************************/
+	void set_percept(string p);
+/*********************************************************************
+** Function: get_sprite
+** Description: pure virtual function 
+** Parameters: char 
+** Pre-Conditions:
+** Post-Conditions:
+*********************************************************************/
+	virtual char get_sprite() = 0;
+/*********************************************************************
+** Function: get_percept
+** Description: pure virtual function  
+** Parameters: char
+** Pre-Conditions: none
+** Post-Conditions: none
+*********************************************************************/
+	virtual string get_percept() = 0;
+	//Polymorphic Function 
+	virtual void action() = 0;
 };
 #endif
