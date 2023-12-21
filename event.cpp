d4c0800f8174d1f45632ccef0ae2ca8cc16af6b5
Drones507 <114556741+Drones507@users.noreply.github.com>
2023-12-20 16:36:36 -1000

    Update event.cpp

--- event.cpp
+++ event.cpp
@@ -1,7 +1,23 @@
 #include "event.h"
 
-#include <iostream>
-
 using namespace std;
+//Default Constructor
+Event::Event(char e)
+	: sprite(e){
+}
+void Event::set_sprite(char e){
+	sprite = e;
+}
+void Event::set_percept(string p){
+	percept = p;
+}
+char Event::get_sprite(){
+	return sprite;
+}
+string Event::get_percept(){
+	return percept;
+}
 
-//Event Implementation
+void action(){
+	return;
+}
