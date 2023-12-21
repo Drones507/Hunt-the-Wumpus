48505d52fa94841f25e272ddcc6acd63f77db114
Drones507 <114556741+Drones507@users.noreply.github.com>
2023-12-20 16:35:35 -1000

    Updated bats.h

--- bats.h
+++ bats.h
@@ -2,10 +2,14 @@
 #define BATS_H 
 
 #include "event.h"
+using namespace std;
 
-//Bats Interface
 class Bats : public Event{
 	public:
+		bool confused = false; 
+		Bats(char B);
+		void action() override;
+		char get_sprite();
+		string get_percept();
 };
-
 #endif
