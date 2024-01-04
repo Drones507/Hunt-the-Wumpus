6920b91a7c9c88eef7fdaee93f4c752d5942ec44
Drones507 <114556741+Drones507@users.noreply.github.com>
2024-01-03 14:41:54 -1000

    Update gold.cpp

--- gold.cpp
+++ gold.cpp
@@ -1,7 +1,14 @@
-#include "gold.h"
-
 #include <iostream>
+#include "gold.h"
 
 using namespace std;
 
-//Gold Implementation
+Gold::Gold(char G, string percpt)
+	: Event(G, percpt){
+	}
+char Gold::get_sprite(){
+	return sprite;
+}
+string Gold::get_percept(){
+	return percept;
+}
