4e6ed30594d39338cd791f1802a04da8e69f9244
Drones507 <114556741+Drones507@users.noreply.github.com>
2024-01-03 14:40:26 -1000

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -3,6 +3,7 @@
 #include "stalactites.h"
 #include "bats.h"
 #include "wumpus.h"
+#include "armor.h"
 #include <vector>
 #include <iostream>
 
@@ -11,10 +12,16 @@ using namespace std;
 //Game Implementation
 
 Game::Game(){
+	// player_y = 0;
+	// player_x = 0;
+	// this->gold_x = 0;
+	// this->gold_y = 0;
+	// this->wumpus_x = 0;
+	// this->wumpus_y = 0;
 }
 
 Game::~Game(){
-	
+
 }
 
 void Game::set_up(int w, int l){
@@ -32,41 +39,186 @@ void Game::set_player_x(){
 void Game::set_player_y() {
 	this->player_y = rand()%this->length;
 } 
-//void Game::set_bats(){
-//	int bat_x, bat_y;
-//	bool check = true;
-//	bat_x = rand()%this->width;
-//	bat_y = rand()%this->length;
-//	while(check == true){
-//		if(myvect[bat_y][bat_x].get_event() != nullptr){
-//			bat_x = rand()%this->width;
-//			bat_y = rand()%this->length;
-//		}
-//		else{
-//			check = false;
-//		}
-//	}
-//	myvect[bat_y][bat_x].set_event(new Bats('B'));
-//}
 
+void Game::set_bats(){
+	bool check = true;
+	bat_x = rand()%this->width;
+	bat_y = rand()%this->length;
+	while(check == true){
+		if(myvect[bat_y][bat_x].get_event() != nullptr){
+			bat_x = rand()%this->width;
+			bat_y = rand()%this->length;
+		}
+		else{
+			check = false;
+		}
+	}
+	myvect[bat_y][bat_x].set_event(new Bats('B', "You hear wings flapping nearby...\n"));
+}
+void Game::set_stalactites(){
+	bool check = true;
+	stal_x = rand()%this->width;
+	stal_y = rand()%this->length;
+	while(check == true){
+		if(myvect[stal_y][stal_x].get_event() != nullptr){
+			stal_x = rand()%this->width;
+			stal_y = rand()%this->length;
+		}
+		else{
+			check = false;
+		}
+	}
+	myvect[stal_y][stal_x].set_event(new Stalactites('S', "You hear water dripping nearby...\n"));
+}
+void Game::set_armor(){
+	bool check = true;
+	armor_x = rand()%this->width;
+	armor_y = rand()%this->length;
+	while(check == true){
+		if(myvect[armor_y][armor_x].get_event() != nullptr){
+			armor_x = rand()%this->width;
+			armor_y = rand()%this->length;
+		}
+		else{
+			check = false;
+		}
+	}
+	myvect[armor_y][armor_x].set_event(new Armor('A', "You feel the power...\n"));
+}
+void Game::set_gold(){
+	bool check = true;
+	gold_x = rand()%this->width;
+	gold_y = rand()%this->length;
+	while(check == true){
+		if(myvect[gold_y][gold_x].get_event() != nullptr){
+			gold_x = rand()%this->width;
+			gold_y = rand()%this->length;
+		}
+		else{
+			check = false;
+		}
+	}
+	myvect[gold_y][gold_x].set_event(new Gold('G', "You see something glimmering nearby...\n"));
+}
+void Game::set_wumpus(){
+	// int wumpus_x, wumpus_y;
+	bool check = true;
+	wumpus_x = rand()%this->width;
+	wumpus_y = rand()%this->length;
+	while(check == true){
+		if(myvect[wumpus_y][wumpus_x].get_event() != nullptr){
+			wumpus_x = rand()%this->width;
+			wumpus_y = rand()%this->length;
+		}
+		else{
+			check = false;
+		}
+	}
+	myvect[wumpus_y][wumpus_x].set_event(new Wumpus('W', "You smell a horrible stench...\n"));
+}
 
-
-//void Game::add_bats(){
-//	for(int i=0;i<2;i++){
-//		set_bats();
-//	}
-//}
-
-//void Game::percept(){
- //	if(player_x && player_y == bat_x && bat_y){
- //		cout << e.get_percept();
-//		}
-//}
-
+void Game::add_bats(){
+	for(int i=0;i<2;i++){
+		set_bats();
+	}
+}
+void Game::add_stalactites(){
+	for(int i=0;i<2;i++){
+		set_stalactites();
+	}
+}
+void Game::place_events(){
+	add_bats();
+	add_stalactites();
+	set_armor();
+	set_gold();
+	set_wumpus();
+}
+void Game::armor_percept(){
+	if((player_x == armor_x+1) && (player_y == armor_y)){
+		cout << myvect[armor_y][armor_x].get_string();
+		}
+	else if((player_x == armor_x-1) && (player_y == armor_y)){
+		cout << myvect[armor_y][armor_x].get_string();
+		}
+	else if((player_x == armor_x) && (player_y == armor_y-1)){
+		cout << myvect[armor_y][armor_x].get_string();
+		}
+	else if((player_x == armor_x) && (player_y == armor_y+1)){
+		cout << myvect[armor_y][armor_x].get_string();
+		}
+}
+void Game::bat_percept(){
+	if((player_x == bat_x+1) && (player_y == bat_y)){
+		cout << myvect[bat_y][bat_x].get_string();
+		}
+	else if((player_x == bat_x-1) && (player_y == bat_y)){
+		cout << myvect[bat_y][bat_x].get_string();
+		}
+	else if((player_x == bat_x) && (player_y == bat_y-1)){
+		cout << myvect[bat_y][bat_x].get_string();
+		}
+	else if((player_x == bat_x) && (player_y == bat_y+1)){
+		cout << myvect[bat_y][bat_x].get_string();
+		}
+}	
+void Game::gold_percept(){
+	if((player_x == gold_x+1) && (player_y == gold_y)){
+		cout << myvect[gold_y][gold_x].get_string();
+		}
+	else if((player_x == gold_x-1) && (player_y == gold_y)){
+		cout << myvect[gold_y][gold_x].get_string();
+		}
+	else if((player_x == gold_x) && (player_y == gold_y-1)){
+		cout << myvect[gold_y][gold_x].get_string();
+		}
+	else if((player_x == gold_x) && (player_y == gold_y+1)){
+		cout << myvect[gold_y][gold_x].get_string();
+		}
+}
+void Game::stal_percept(){
+	if((player_x == stal_x+1) && (player_y == stal_y)){
+		cout << myvect[stal_y][stal_x].get_string();
+		}
+	else if((player_x == stal_x-1) && (player_y == stal_y)){
+		cout << myvect[stal_y][stal_x].get_string();
+		}
+	else if((player_x == stal_x) && (player_y == stal_y-1)){
+		cout << myvect[stal_y][stal_x].get_string();
+		}
+	else if((player_x == stal_x) && (player_y == stal_y+1)){
+		cout << myvect[stal_y][stal_x].get_string();
+		}
+}
+void Game::wumpus_percept(){
+	if((player_x == wumpus_x+1) && (player_y == wumpus_y)){
+		cout << myvect[wumpus_y][wumpus_x].get_string();
+		}
+	else if((player_x == wumpus_x-1) && (player_y == wumpus_y)){
+		cout << myvect[wumpus_y][wumpus_x].get_string();
+		}
+	else if((player_x == wumpus_x) && (player_y == wumpus_y-1)){
+		cout << myvect[wumpus_y][wumpus_x].get_string();
+		}
+	else if((player_x == wumpus_x) && (player_y == wumpus_y+1)){
+		cout << myvect[wumpus_y][wumpus_x].get_string();
+		}
+}
 void Game::display_game(bool d) {
+	//Store events in a room ((A vector container))
+	//Create a display_events function
+	//Display Events polymorphically with display_events function
+	//Make sure to free memory
 	cout << endl << endl;
 	cout << "Arrows remaining: " << this->num_arrows << endl;
 	cout << this->debug_view << endl;
+	cout << "\n Wumpus: " << wumpus_x << "," << wumpus_y << endl;
+	cout << "\n Gold: " << gold_x << "," << gold_y << endl;
+	armor_percept();
+	bat_percept();
+	gold_percept();
+	stal_percept();
+	wumpus_percept();
 	string line = "";
 	for (int i = 0; i < this->width; ++i)
 		line += "-----";
@@ -75,10 +227,10 @@ void Game::display_game(bool d) {
 		cout << line << endl;
 		for (int j = 0; j < this->width; ++j)
 		{
-			if (myvect[i][j].check_event()==true){
-			 	cout << myvect[i][j].get_sprite();
-			}
-			if(player_x == j&& player_y == i){
+			// if (myvect[i][j].check_event()==true){
+			//  	cout << myvect[i][j].get_char();
+			// } Written by TA during offic hours 		
+			if(player_x == j && player_y == i){
 			   cout << "*"; 
 			}else{
 				cout << " ";
@@ -87,20 +239,14 @@ void Game::display_game(bool d) {
 				cout << "  ||";
 			}
 			else{
-				//Debug game mode
+				//Debug game mode. Prints event sprites as characters
 				if(this->debug_view == true){
-					cout << myvect[i][j].get_sprite();
+					cout << myvect[i][j].get_char();
 					cout << " ||";
-				//print the corresponding char of the event
 				}
 				//Regular game mode
 				else{
-					if(player_x == j&& player_y == i){
-					cout << "*"; 
-					} else{
-					cout << " ";
-					}
-					cout << " ||";
+					cout << "  ||";
 				}
 			}
 		}
@@ -284,39 +430,23 @@ char Game::get_input(){
 
 	return c;
 }
-
-
 //Note: you need to modify this function
 void Game::play_game(int w, int l, bool d){
-
 	Game::set_up(w, l);
 	this->debug_view = d;
 	char input;
 	set_player_x();
 	set_player_y();
-	add_bats();
-	//int escaperope[2] = {set_player_x, set_player_y}; //const
-
+	place_events();
 	while (Game::check_win() == false){
 		//print game board
-		cout << player_x<< endl;
-		cout << player_y<< endl;
 		Game::display_game(this->debug_view);
 		//display percerts around player's location
-		//Your code here:
-
-		//Player move...
-		//1. get input
 		input = Game::get_input();
-	
-		//2. move player
 		Game::move(input);
-
 		//3. may or may not encounter events
-		//Percept
-		// Game::percept();
+// Game::percept();
 		//Action
 	}
 	return;
 }
-
