75ec921f264b71c251dad314ca037c62e1a56067
Drones507 <114556741+Drones507@users.noreply.github.com>
2024-01-03 14:49:15 -1000

    Update wumpus.cpp

--- wumpus.cpp
+++ wumpus.cpp
@@ -1,7 +1,13 @@
 #include "wumpus.h"
-
 #include <iostream>
-
 using namespace std;
 
-//Wumpus Implementation
+Wumpus::Wumpus(char W, string percpt)
+	: Event(W, percpt){
+	}
+char Wumpus::get_sprite(){
+	return sprite;
+}	
+string Wumpus::get_percept(){
+	return percept;
+}
